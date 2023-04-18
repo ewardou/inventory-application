8a9882a0547f7abf6c6a0b5f2ec21761303a5ccd
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-04-18 13:06:32 -0400

    Add route handler functions updateGame and deleteGame

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -10,10 +10,33 @@ exports.getGameDetails = asyncHandler(async (req, res) => {
     const [game] = await Games.find({ _id: req.params.id })
         .populate('availableConsoles')
         .exec();
-    console.log(game);
     res.render('game_detail', { game });
 });
 
 exports.createGame = (req, res) => {
     res.render('game_create');
 };
+
+exports.updateGame = asyncHandler(async (req, res) => {
+    const [game] = await Games.find({ _id: req.params.id })
+        .populate('availableConsoles')
+        .exec();
+    const checked = {
+        nintendo: false,
+        xbox: false,
+        playstation: false,
+    };
+    game.availableConsoles.forEach((console) => {
+        Object.keys(checked).forEach((key) => {
+            if (console.name.toLowerCase().includes(key)) {
+                checked[key] = true;
+            }
+        });
+    });
+    res.render('game_create', { game, checked });
+});
+
+exports.deleteGame = asyncHandler(async (req, res) => {
+    await Games.findByIdAndRemove(req.params.id);
+    res.redirect('/games');
+});
--- routes/gamesRoute.js
+++ routes/gamesRoute.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.get('/games', gamesController.getGames);
 router.get('/games/create', gamesController.createGame);
 router.get('/games/:id', gamesController.getGameDetails);
+router.get('/games/:id/update', gamesController.updateGame);
+router.post('/games/:id/delete', gamesController.deleteGame);
 
 module.exports = router;
