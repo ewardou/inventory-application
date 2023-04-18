1b89145cb9fa69952c1257f3f57ce667e08c303d
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-04-18 15:51:51 -0400

    Modify game_form template to make consoles section dynamic

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const Games = require('../models/games');
+const Consoles = require('../models/consoles');
 
 exports.getGames = asyncHandler(async (req, res) => {
     const allGames = await Games.find().populate('availableConsoles').exec();
@@ -13,27 +14,27 @@ exports.getGameDetails = asyncHandler(async (req, res) => {
     res.render('game_detail', { game });
 });
 
-exports.createGame = (req, res) => {
-    res.render('game_form');
-};
+exports.createGame = asyncHandler(async (req, res) => {
+    const allConsoles = await Consoles.find().exec();
+    res.render('game_form', {
+        consoles: allConsoles,
+        game: undefined,
+    });
+});
 
 exports.updateGame = asyncHandler(async (req, res) => {
-    const [game] = await Games.find({ _id: req.params.id })
-        .populate('availableConsoles')
-        .exec();
-    const checked = {
-        nintendo: false,
-        xbox: false,
-        playstation: false,
-    };
-    game.availableConsoles.forEach((console) => {
-        Object.keys(checked).forEach((key) => {
-            if (console.name.toLowerCase().includes(key)) {
-                checked[key] = true;
+    const [game, consoles] = await Promise.all([
+        Games.findById(req.params.id).exec(),
+        Consoles.find().exec(),
+    ]);
+    game.availableConsoles.forEach((availableConsole) => {
+        consoles.forEach((console) => {
+            if (console._id.toString() === availableConsole.toString()) {
+                console.checked = true;
             }
         });
     });
-    res.render('game_form', { game, checked });
+    res.render('game_form', { game, consoles });
 });
 
 exports.deleteGame = asyncHandler(async (req, res) => {
