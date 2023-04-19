e9679cfea1261e810a2b1573c43de722a9f6f646
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-04-19 17:05:16 -0400

    Add route handler functions for "accessory/create"

--- controllers/accessoriesController.js
+++ controllers/accessoriesController.js
@@ -19,3 +19,43 @@ exports.getAccessoryDetails = asyncHandler(async (req, res) => {
         .exec();
     res.render('game_detail', { game: accessory });
 });
+
+exports.createAccessory = asyncHandler(async (req, res) => {
+    const allConsoles = await Consoles.find().exec();
+    res.render('accessory_form', {
+        consoles: allConsoles,
+        accessory: undefined,
+        errors: null,
+    });
+});
+
+exports.createAccessory_Post = [
+    body('price').trim().isInt({ min: 1 }).escape(),
+    body('quantity').trim().isInt({ min: 1 }).escape(),
+    body('name').trim().isLength({ min: 1, max: 100 }).escape(),
+    body('availableConsoles').optional().trim(),
+    asyncHandler(async (req, res) => {
+        const errors = validationResult(req);
+        const consoles = await Consoles.find({}).exec();
+        if (!req.body.availableConsoles) {
+            req.body.availableConsoles = consoles.map((console) => console._id);
+        }
+        const accessory = new Accessories({
+            name: req.body.name,
+            price: req.body.price,
+            quantity: req.body.quantity,
+            availableConsoles: req.body.availableConsoles,
+        });
+
+        if (!errors.isEmpty()) {
+            res.render('accessory_form', {
+                accessory,
+                consoles,
+                errors: errors.array(),
+            });
+        } else {
+            await accessory.save();
+            res.redirect('/accessories');
+        }
+    }),
+];
--- routes/accessoriesRoute.js
+++ routes/accessoriesRoute.js
@@ -4,6 +4,8 @@ const accessoriesController = require('../controllers/accessoriesController');
 const router = express.Router();
 
 router.get('/accessories', accessoriesController.getAccessories);
+router.get('/accessories/create', accessoriesController.createAccessory);
+router.post('/accessories/create', accessoriesController.createAccessory_Post);
 router.get('/accessories/:id', accessoriesController.getAccessoryDetails);
 
 module.exports = router;
