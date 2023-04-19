6504b00ed9d579f5bc62ce0b3ded5ccad58a1b75
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-04-19 15:41:28 -0400

    Write create and update func for POST method

--- controllers/consolesController.js
+++ controllers/consolesController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 const Consoles = require('../models/consoles');
 const Games = require('../models/games');
 
@@ -16,13 +17,66 @@ exports.getConsoleDetails = asyncHandler(async (req, res) => {
 });
 
 exports.createConsole = (req, res) => {
-    res.render('console_form');
+    res.render('console_form', { errors: null });
 };
+
+exports.createConsole_Post = [
+    body('price').isInt({ min: 1 }).escape(),
+    body('quantity').isInt({ min: 1 }).escape(),
+    body('name').trim().isLength({ min: 1, max: 100 }).escape(),
+    body('manufacturer').trim().isLength({ min: 1, max: 100 }).escape(),
+    asyncHandler(async (req, res) => {
+        const errors = validationResult(req);
+        const gameConsole = new Consoles({
+            name: req.body.name,
+            price: req.body.price,
+            quantity: req.body.quantity,
+            manufacturer: req.body.manufacturer,
+        });
+
+        if (!errors.isEmpty()) {
+            res.render('console_form', {
+                console: gameConsole,
+                errors: errors.array(),
+            });
+        } else {
+            await gameConsole.save();
+            res.redirect('/consoles');
+        }
+    }),
+];
 
 exports.updateConsole = asyncHandler(async (req, res) => {
     const gameConsole = await Consoles.findById(req.params.id).exec();
-    res.render('console_form', { console: gameConsole });
+    res.render('console_form', { console: gameConsole, errors: null });
 });
+
+exports.updateConsole_Post = [
+    body('price').isInt({ min: 1 }).escape(),
+    body('quantity').isInt({ min: 1 }).escape(),
+    body('name').trim().isLength({ min: 1, max: 100 }).escape(),
+    body('manufacturer').trim().isLength({ min: 1, max: 100 }).escape(),
+    asyncHandler(async (req, res) => {
+        const errors = validationResult(req);
+        const gameConsole = new Consoles({
+            name: req.body.name,
+            price: req.body.price,
+            quantity: req.body.quantity,
+            manufacturer: req.body.manufacturer,
+            _id: req.params.id,
+        });
+
+        if (!errors.isEmpty()) {
+            res.render('console_form', {
+                console: gameConsole,
+                errors: errors.array(),
+            });
+        } else {
+            await Consoles.findByIdAndUpdate(req.params.id, gameConsole, {});
+            res.redirect('/consoles');
+        }
+    }),
+];
 
 exports.deleteConsole = asyncHandler(async (req, res) => {
     const games = await Games.find({ availableConsoles: req.params.id }).exec();
--- routes/consolesRoute.js
+++ routes/consolesRoute.js
@@ -5,8 +5,10 @@ const router = express.Router();
 
 router.get('/consoles', consolesController.getConsoles);
 router.get('/consoles/create', consolesController.createConsole);
+router.post('/consoles/create', consolesController.createConsole_Post);
 router.get('/consoles/:id', consolesController.getConsoleDetails);
 router.get('/consoles/:id/update', consolesController.updateConsole);
+router.post('/consoles/:id/update', consolesController.updateConsole_Post);
 router.post('/consoles/:id/delete', consolesController.deleteConsole);
 
 module.exports = router;
